497b5bb471fe6d9373bf4e8b50ddce393ad39cbf
Tigran <50572613+babayanta1994@users.noreply.github.com>
2026-03-25 18:30:07 +0300

    Update autoXRAY1.sh

--- autoXRAY1.sh
+++ autoXRAY1.sh
@@ -63,7 +63,7 @@ WEB_PATH="/var/www/$DOMAIN"
 mkdir -p "$WEB_PATH"
 
 # Генерируем сайт маскировку
-bash -c "$(curl -L https://github.com/xVRVx/autoXRAY/raw/refs/heads/main/test/gen_page2.sh)" -- $WEB_PATH
+bash -c "$(curl -L https://github.com/babayanta1994/autoXRAY/raw/refs/heads/main/test/gen_page2.sh)" -- $WEB_PATH
 
 # Установка Xray
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
@@ -1008,7 +1008,7 @@ cat >> "$WEB_PATH/$path_subpage.html" <<EOF
     <button class="btn-action qr-btn" onclick="showQR('cAll')">QR</button>
 </div>
 
-<div><a style="color:white;margin:40px auto 20px;display:block;text-align:center;" href="https://github.com/xVRVx/autoXRAY">https://github.com/xVRVx/autoXRAY</a></div>
+<div><a style="color:white;margin:40px auto 20px;display:block;text-align:center;" href="https://github.com/babayanta1994/autoXRAY">https://github.com/babayanta1994/autoXRAY</a></div>
 
 <div id="qrModal" class="modal-overlay"><div class="modal-content"><div id="qrcode"></div><button class="close-modal-btn" onclick="closeModal()">Close</button></div></div>
 </body></html>
@@ -1064,6 +1064,6 @@ ${GRN}$configListLink ${NC}
 
 Открыт локальный socks5 на порту 10808, 2080 и http на 10809.
 
-${GRN}Поддержать автора: https://github.com/xVRVx/autoXRAY ${NC}
+${GRN}Поддержать автора: https://github.com/babayanta1994/autoXRAY ${NC}
 
 "
